d5400744711466b20129a5a9c36b471a67c465fc
ywci <root@wing-virtual-machine.(none)>
2015-02-08 22:22:36 +0800

    Add installer

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+apt-get install -y build-essential
+apt-get install -y libopencv-dev
+apt-get install -y libzbar-dev
+apt-get install -y libzmq-dev
+apt-get install -y python-pip
+apt-get install -y python-pil
+apt-get install -y python-dbus
+apt-get install -y python-cffi
+apt-get install -y python-numpy
+apt-get install -y python-gevent
+apt-get install -y python-gi-dev
+apt-get install -y python-all-dev
+apt-get install -y python-imaging
+apt-get install -y python-pygame
+apt-get install -y python-opencv
+apt-get install -y python-zbar
+pip install wget
+pip install pypng
+pip install pyqrcode
+pip install hash_ring
+pip install isodate
+pip install netifaces
+pip install pyzmq
+pip install zerorpc
+pip install xattr
+pip install psutil
+pip install RestrictedPython
+pip install pycrypto
